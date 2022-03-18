76f0131cc2cff6debef117f6477689838020cfff
Katie Breuil <kcbreuil@gmail.com>
2022-03-18 19:36:36 -0400

    add about section

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ContactForm from "./contactForm"
+
+export default function About() {
+  return (
+    <section id="about">
+      <h2 className="uppercase border border-white rounded p-4 max-w-max my-10">
+        About
+      </h2>
+      <p></p>
+    </section>
+  )
+}
